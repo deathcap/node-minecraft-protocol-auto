4190a4a8d80a68be311e7549ffdcef766ea87cf2
deathcap <deathcap@gmx.co.uk>
2016-01-24 12:54:43 -0800

    Change createClient to asynchronous createClientAsync API
    One problem with synchronous createClient() is we don't necessarily know
    which client code to use before receiving the ping response. Supposedly
    it is safe to use Forge clients to connect to vanilla servers, so that's
    not a very big problem. The bigger issue is that for servers that do not
    respond to ping packets [1], the synchronous createClient() called
    before the ping will still continue executing -- i.e., a race condition.
    
    [1] https://github.com/PrismarineJS/node-minecraft-protocol/issues/327#issuecomment-174310946

--- auto.js
+++ auto.js
@@ -1,32 +1,36 @@
 'use strict';
 
 var mcf = require('minecraft-protocol-forge');
-var ping = require('minecraft-protocol').ping;
+var mc = require('minecraft-protocol');
+var ping = mc.ping;
 var assert = require('assert');
 
-function createClientAuto(options) {
+function createClientAuto(options, cb) {
   assert.ok(options, 'options is required');
-  var client = mcf.createClient(options); // try to use Forge, it is safe even on vanilla
 
+  console.log('pinging',options.host);
+  // TODO: refactor with DNS SRV lookup in NMP
   ping(options, function(err, response) {
-    if (err) throw err;
+    var client;
+
+    if (err) return cb(err, null);
     console.log('ping response',response);
     // TODO: could also use ping pre-connect to save description, type, negotiate protocol etc.
     //  ^ see https://github.com/PrismarineJS/node-minecraft-protocol/issues/327
 
-    if (response.modinfo) {
-      if (response.modinfo.type === 'FML') {
-        // Use the list of Forge mods from the server ping, so client will match server
-        var forgeMods = response.modinfo.modList;
-        console.log('Using forgeMods:',forgeMods);
-        client.forgeMods = forgeMods; // for fmlHandshakeStep TODO: refactor
-      }
+    if (response.modinfo && response.modinfo.type === 'FML') {
+      // Use the list of Forge mods from the server ping, so client will match server
+      var forgeMods = response.modinfo.modList;
+      console.log('Using forgeMods:',forgeMods);
+      client = mcf.createClient(options); // we know we can use Forge
+      client.forgeMods = forgeMods; // for fmlHandshakeStep TODO: refactor, constructor?
+    } else {
+        client = mc.createClient(options); // vanilla
     }
+    cb(null, client);
   });
-
-  return client;
 }
 
 module.exports = {
-  createClient: createClientAuto
+  createClientAsync: createClientAuto
 };
--- demo.js
+++ demo.js
@@ -13,24 +13,26 @@ var port = parseInt(process.argv[3]);
 var username =  process.argv[4] ? process.argv[4] : "echo";
 var password = process.argv[5];
 
-var client = mc.createClient({host, port, username, password});
-if (!client) {
-  throw new Error('failed to create client');
-}
+mc.createClientAsync({host, port, username, password}, function(err, client) {
+  if (err) {
+    console.log('failed to create client', err);
+    throw err;
+  }
 
-client.on('connect', function() {
-  console.info('connected');
-});
-client.on('disconnect', function(packet) {
-  console.log('disconnected: '+ packet.reason);
-});
-client.on('end', function(err) {
-  console.log('Connection lost');
-});
-client.on('chat', function(packet) {
-  console.log('received chat',packet);
-});
+  client.on('connect', function() {
+    console.info('connected');
+  });
+  client.on('disconnect', function(packet) {
+    console.log('disconnected: '+ packet.reason);
+  });
+  client.on('end', function(err) {
+    console.log('Connection lost');
+  });
+  client.on('chat', function(packet) {
+    console.log('received chat',packet);
+  });
 
-client.on('forgeMods', function(mods) {
-  console.log('Received forgeMods event:',mods);
+  client.on('forgeMods', function(mods) {
+    console.log('Received forgeMods event:',mods);
+  });
 });
